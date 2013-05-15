1d9550b60964f50cf0aa70a277db2e4d6ead45da
Benjamin Jones <bjones@galois.com>
2013-05-15 11:21:41 -0700

    updates to Trac 4102 patches

---
+++ trac_symbolic_bessel_apply.sh
@@ -0,0 +1,6 @@
+hg qimport http://trac.sagemath.org/sage_trac/raw-attachment/ticket/4102/trac_symbolic_bessel_v5.patch
+hg qpush
+hg qimport http://trac.sagemath.org/sage_trac/raw-attachment/ticket/4102/trac_symbolic_bessel_doctests.patch
+hg qpush
+hg qimport http://trac.sagemath.org/sage_trac/raw-attachment/ticket/4102/trac_symbolic_bessel_deprecation.patch
+hg qpush
